a38013023aabe66ad704cc0c4e6d9901a7946a3d
DGK <pasito.dev@gmail.com>
2022-08-18 11:00:19 +0900

    feat: 유저 로그아웃 기능 구현 (#5)

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from users.views.user_signup import UserSignUpView
-from users.views.user_signin import UserSignInView
+from users.views.user_signup  import UserSignUpView
+from users.views.user_signin  import UserSignInView
+from users.views.user_signout import UserSignOutView
 
 
 """
-유저 회원가입/로그인 url patterns
+유저 회원가입/로그인/로그아웃 url patterns
 """
 urlpatterns = [
     path('/signup', UserSignUpView.as_view()),
     path('/signin', UserSignInView.as_view()),
+    path('/signout', UserSignOutView.as_view()),
 ]
---
+++ users/views/user_signout.py
@@ -0,0 +1,57 @@
+from rest_framework.views            import APIView
+from rest_framework.permissions      import IsAuthenticated
+from rest_framework.response         import Response
+from rest_framework_simplejwt.tokens import OutstandingToken, BlacklistedToken, RefreshToken
+
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg       import openapi
+
+
+class UserSignOutView(APIView):
+    """
+    Assignee: 김동규
+    
+    request body: refresh token
+    return: json
+    detail:
+      - 유저 로그아웃 기능입니다.
+      - 토큰의 타입이 유효한지 확인합니다.(리프레시 토큰)
+      - 토큰의 유저정보가 API 요청자의 정보와 일치하는지 확인합니다.
+      - 모든 유효성 검사에 통과하면, 해당 유저의 모든 리프레시 토큰 사용을 제한합니다.(토큰 블랙리스트 활용)
+    """
+    
+    permission_classes = [IsAuthenticated]
+    
+    post_params = openapi.Schema(type=openapi.TYPE_OBJECT, properties={
+        'refesh_token': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        }
+    )
+    
+    @swagger_auto_schema(request_body=post_params, responses={200: '유저가 로그아웃 되었습니다.'})
+    def post(self, request):
+        """
+        POST: 유저 로그아웃 기능
+        """
+        user = request.user
+        
+        """
+        입력한 리프레시 토큰의 정보를 가져옵니다.
+        """
+        try:
+            refresh = RefreshToken(request.data['refesh_token'])
+        except:
+            return Response({'detail': '유효하지 않거나 만료된 토큰입니다.'}, status=400)
+        
+        """
+        입력한 리프레시 토큰이 본인(API 요청자)의 토큰인지를 확인합니다.
+        """
+        if not user.id == refresh['user_id']:
+            return Response({'detail': '유저의 토큰정보가 유효하지 않습니다.'}, status=400)
+        
+        """
+        해당 유저의 발급된 모든 리프레시 토큰을 사용 제한합니다.
+        """
+        for token in OutstandingToken.objects.filter(user_id=refresh['user_id']):
+            BlacklistedToken.objects.get_or_create(token=token)
+            
+        return Response({'message' : f'유저 {user.nickname}이 로그아웃 되었습니다.'}, status=200)
